e8ec9e3992b9bfc353fd690e43332ab0790bfa0c
seoyoungsoo <iulove37@naver.com>
2021-06-18 17:31:35 +0900

    Style: 헤더 스타일 추가

---
+++ layouts/Header/styles.jsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const Head = styled.div`
+  height: 174px;
+  width: 100%;
+  display: block;
+`;
+
+export const HeaderTop = styled.header`
+  height: 120px;
+  width: 100%;
+  display: flex;
+  --saf-0: rgba(var(--sk_foreground_low, 29, 28, 29), 0.13);
+  box-shadow: 0 1px 0 var(--saf-0);
+  padding: 20px 16px 20px 20px;
+  font-weight: bold;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+`;
