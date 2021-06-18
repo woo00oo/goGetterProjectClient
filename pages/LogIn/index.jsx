03814f7eb218fe26186e0fcd95a1a54b6322087b
seoyoungsoo <iulove37@naver.com>
2021-06-18 17:35:58 +0900

    Feat: 로그인 기본 구조

--- pages/LogIn/index.jsx
+++ pages/LogIn/index.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import Header from '@layouts/Header';
+import { LoginDiv, Form, Login, Input, Button, Search, Social, FaceBook, Google } from '@pages/LogIn/styles';
+import { Link } from 'react-router-dom';
 
-const Login = () => {
-  return <div>로그인 화면입니다.</div>;
+const LogIn = () => {
+  return (
+    <div id="container">
+      <Header />
+      <LoginDiv>
+        <Login>
+          <h2>로그인</h2>
+          <Form onSubmit>
+            <span>
+              <Link to="/signup">우도독 가입하기</Link>
+            </span>
+            <div id="email">
+              <Input type="email" id="email" name="email" placeholder="이메일" />
+            </div>
+            <div id="password">
+              <Input type="password" id="password" name="password" placeholder="비밀번호" />
+            </div>
+            <Button type="submit">로그인</Button>
+          </Form>
+        </Login>
+        <Search>
+          <span>
+            <Link to="/">아이디 찾기</Link>|<Link to="/">비밀번호 찾기</Link>
+          </span>
+        </Search>
+        <Social>
+          <FaceBook>
+            <i class="fab fa-facebook-square"></i> 페이스북으로 시작하기
+          </FaceBook>
+          <Google>
+            <i class="fab fa-google"></i> 구글로 시작하기
+          </Google>
+        </Social>
+      </LoginDiv>
+    </div>
+  );
 };
 
-export default Login;
+export default LogIn;
