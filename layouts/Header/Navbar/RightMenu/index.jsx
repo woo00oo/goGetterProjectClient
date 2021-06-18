56f3e8090201f1605d6003626d501d88e488e23d
seoyoungsoo <iulove37@naver.com>
2021-06-18 17:33:05 +0900

    Feat: NavBar RightMenu 기능

---
+++ layouts/Header/Navbar/RightMenu/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Right } from '@layouts/Header/Navbar/RightMenu/styles';
+
+const RightMenu = () => {
+  return <Right>RightMenu</Right>;
+};
+
+export default RightMenu;
