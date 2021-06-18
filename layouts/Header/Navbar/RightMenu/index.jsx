51b5942e86f6383cf41712295bed545b3cdb94d4
seoyoungsoo <iulove37@naver.com>
2021-06-18 18:36:31 +0900

    Feat #noissue: RightMenu 기능

--- layouts/Header/Navbar/RightMenu/index.jsx
+++ layouts/Header/Navbar/RightMenu/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Right } from '@layouts/Header/Navbar/RightMenu/styles';
+import { Link } from 'react-router-dom';
 
 const RightMenu = () => {
-  return <Right>RightMenu</Right>;
+  return (
+    <div id="rightmenu">
+      <Right>
+        <span>
+          <Link to="/">로그인</Link>
+        </span>
+        <i class="fas fa-search"></i>
+        <input type="text" />
+      </Right>
+    </div>
+  );
 };
 
 export default RightMenu;
