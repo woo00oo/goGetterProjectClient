4b3bce48006d5bcd155889d912475e66b5e68aa9
seoyoungsoo <iulove37@naver.com>
2021-06-18 18:35:17 +0900

    Refactor: LeftMenu 구조 변경

--- layouts/Header/Navbar/LeftMenu/index.jsx
+++ layouts/Header/Navbar/LeftMenu/index.jsx
@@ -4,22 +4,24 @@ import { Link } from 'react-router-dom';
 
 const LeftMenu = () => {
   return (
-    <Left>
-      <ul>
-        <li>
-          <Link to="/">독서 공유</Link>
-        </li>
-        <li>
-          <Link to="/">토론 게시판</Link>
-        </li>
-        <li>
-          <Link to="/">도서관 찾기</Link>
-        </li>
-        <li>
-          <Link to="/">이벤트</Link>
-        </li>
-      </ul>
-    </Left>
+    <div id="leftmenu">
+      <Left>
+        <ul>
+          <li>
+            <Link to="/">독서 공유</Link>
+          </li>
+          <li>
+            <Link to="/">토론 게시판</Link>
+          </li>
+          <li>
+            <Link to="/">도서관 찾기</Link>
+          </li>
+          <li>
+            <Link to="/">이벤트</Link>
+          </li>
+        </ul>
+      </Left>
+    </div>
   );
 };
 
