2ba7e4f127236a755cce5cf07401c02c090b550c
seoyoungsoo <iulove37@naver.com>
2021-06-18 17:32:30 +0900

    Style: 네비게이션 바 스타일 변경

---
+++ layouts/Header/Navbar/styles.jsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const Fixed = styled.div`
+  position: relative;
+  height: 54px;
+  width: 100%;
+  background-color: #0f4c81;
+`;
+
+export const Inner = styled.div`
+  width: 1200px;
+  height: 100%;
+  display: flex;
+  /* background-color: #939597; */
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  color: #fff;
+  padding: 15px 10px;
+`;
