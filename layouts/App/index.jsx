255bf8364f84df4aa940e8a92aabe720e8f79290
seoyoungsoo <iulove37@naver.com>
2021-06-19 17:03:35 +0900

    Feat #4: 회원가입 화면 라우터 추가

--- layouts/App/index.jsx
+++ layouts/App/index.jsx
@@ -4,6 +4,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 const LogIn = loadable(() => import('@pages/LogIn'));
 const SignUp = loadable(() => import('@pages/SignUp'));
+const SignUp2 = loadable(() => import('@pages/SignUp/SignUp2'));
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Redirect exact path="/" to="/login" />
       <Route path="/login" component={LogIn} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/signup2" component={SignUp2} />
     </Switch>
   );
 };
