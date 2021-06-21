7e60994ac6d47a67c885ec6b50fe9984348ca1b1
seoyoungsoo <iulove37@naver.com>
2021-06-21 21:19:01 +0900

    Feat #17: 마이페이지 프로필 구현

---
+++ pages/MyPage/Profile/index.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Header from '@layouts/Header';
+import Footer from '@layouts/Footer';
+import Tab from '@components/Tab';
+import {
+  AllDiv,
+  ContentDiv,
+  ProfileDiv,
+  Profile,
+  CantModified,
+  ProfileImage,
+  Label,
+  Input,
+  SmallText,
+  FormGroup,
+  ProfileForm,
+  All,
+} from '@pages/MyPage/Profile/styles';
+
+const MyPageProfile = () => {
+  return (
+    <div id="mypage-profile">
+      <Header />
+      <AllDiv>
+        <Tab />
+        <ContentDiv>
+          <ProfileDiv>
+            <h2>프로필</h2>
+            <Profile>
+              <CantModified>
+                이메일
+                <span>example@gmail.com</span>
+                이름
+                <span>홍길동</span>
+                휴대폰
+                <span>01000000000</span>
+              </CantModified>
+              <ProfileImage>프로필 사진</ProfileImage>
+            </Profile>
+            <ProfileForm>
+              <FormGroup>
+                <Label>닉네임</Label>
+                <br></br>
+                <Input placeholder="닉네임을 입력해주세요." type="text"></Input>
+              </FormGroup>
+              <FormGroup>
+                <Label>비밀번호</Label>
+                <br></br>
+                <Input placeholder="비밀번호를 입력해주세요." type="password"></Input>
+                <SmallText>비밀번호는 8~16자, 영문, 숫자, 특수문자를 포함해야 합니다.</SmallText>
+              </FormGroup>
+              <FormGroup>
+                <Label>비밀번호 확인</Label>
+                <br></br>
+                <Input placeholder="비밀번호를 다시 입력해주세요." type="password"></Input>
+              </FormGroup>
+            </ProfileForm>
+          </ProfileDiv>
+        </ContentDiv>
+      </AllDiv>
+      <Footer />
+    </div>
+  );
+};
+
+export default MyPageProfile;
