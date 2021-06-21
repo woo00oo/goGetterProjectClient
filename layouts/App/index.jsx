164d649fb11131b35bda073a012ce81c8640d9af
DESKTOP-PUHRM72\sodao <sodaoov@gmail.com>
2021-06-21 17:36:58 +0900

    Feat #15: 토론게시판 제목 구현

--- layouts/App/index.jsx
+++ layouts/App/index.jsx
@@ -5,12 +5,14 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 const LogIn = loadable(() => import('@pages/LogIn'));
 const SignUp = loadable(() => import('@pages/SignUp'));
 const SignUp2 = loadable(() => import('@pages/SignUp/SignUp2'));
+const Discussion = loadable(() => import('@pages/Discussion'));
 
 const App = () => {
   return (
     <Switch>
       <Redirect exact path="/" to="/login" />
       <Route path="/login" component={LogIn} />
+      <Route path="/discussion" component={Discussion} />
       <Route path="/signup" component={SignUp} />
       <Route path="/signup2" component={SignUp2} />
     </Switch>
