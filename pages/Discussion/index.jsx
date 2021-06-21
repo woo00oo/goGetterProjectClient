bb02c6befe5a6f44cf3d93ff340789d2818b06d8
DESKTOP-PUHRM72\sodao <sodaoov@gmail.com>
2021-06-21 23:51:09 +0900

    Feat #15: 토론게시판 변경

--- pages/Discussion/index.jsx
+++ pages/Discussion/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import Header from '@layouts/Header';
 import Footer from '@layouts/Footer';
-import { Container, DCHeader } from '@pages/Discussion/styles';
+import {
+  Container,
+  DCHeader,
+  DCSearch,
+  DCTable,
+  DCMain,
+  DCTitle,
+  DCNotice,
+  DCContents,
+  DCButton,
+} from '@pages/Discussion/styles';
 
 const Discussion = () => {
   return (
@@ -9,6 +19,34 @@ const Discussion = () => {
       <Header></Header>
       <Container>
         <DCHeader>토론 게시판</DCHeader>
+        <DCSearch>
+          <select>
+            <option>제목/내용</option>
+            <option>제목</option>
+            <option>내용</option>
+          </select>
+          <i class="fas fa-search"></i>
+          <input placeholder="검색어를 입력해주세요."></input>
+        </DCSearch>
+        <DCTable>
+          <DCTitle>
+            <tr>
+              <th>글번호</th>
+              <th>제목</th>
+              <th>작성자</th>
+              <th>작성날짜</th>
+            </tr>
+          </DCTitle>
+          <DCMain>
+            <tr>
+              <td>1</td>
+              <td>나의 라임 오렌지나무</td>
+              <td>나독자</td>
+              <td>20-06-21</td>
+            </tr>
+          </DCMain>
+        </DCTable>
+        <DCButton>글쓰기</DCButton>
       </Container>
       <Footer></Footer>
     </>
