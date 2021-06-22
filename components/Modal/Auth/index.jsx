b55a2e65f286c384fe9a167e702a20eceea3f929
seoyoungsoo <iulove37@naver.com>
2021-06-22 19:12:59 +0900

    Feat #13: 회원가입 이메일 인증 모달 수정
    모달 클릭 시 인증 메일 발송 버튼 클릭 시 이메일 확인 모달 창이 뜨도록 구현

--- components/Modal/Auth/index.jsx
+++ components/Modal/Auth/index.jsx
@@ -1,27 +1,43 @@
-import React from 'react';
-import { Modal, ModalBtn, ModalWrapper, MainText, SubText, Container } from '@components/Modal/Auth/styles';
+import React, { useState, useCallback } from 'react';
+import { Modal, ModalBtn, ModalWrapper, MainText, SubText, Container, TextInput } from '@components/Modal/Auth/styles';
+import axios from 'axios';
 
-const Auth = ({ authModalOpen, setAuthModalOpen }) => {
+const Auth = ({ authModalOpen, setAuthModalOpen, email }) => {
+  const [sendMail, setSendMail] = useState(false);
+
+  const onChangeModal = useCallback(() => {
+    setAuthModalOpen(false);
+    setSendMail(false);
+  }, []);
+
+  const onChangeSendMail = useCallback(() => {
+    setSendMail(true);
+
+    // axios.post('http://localhost:')
+  }, [email]);
+
   return (
     <>
-      <Modal
-        signUpModal={authModalOpen}
-        onClick={() => {
-          setAuthModalOpen(false);
-        }}
-      />
+      <Modal signUpModal={authModalOpen} onClick={onChangeModal} />
       <Container signUpModal={authModalOpen}>
-        <ModalWrapper>
-          <MainText>입력하신 이메일로 인증을 완료해 주세요.</MainText>
-          <SubText>*수신함에서 인증메일을 찾을 수 없을 경우 스팸함을 조회하세요.</SubText>
-          <ModalBtn
-            onClick={() => {
-              setAuthModalOpen(false);
-            }}
-          >
-            인증 메일 발송
-          </ModalBtn>
-        </ModalWrapper>
+        {!sendMail && (
+          <ModalWrapper>
+            <MainText>입력하신 이메일로 인증을 진행해 주세요.</MainText>
+            <SubText>*수신함에서 인증메일을 찾을 수 없을 경우 스팸함을 조회하세요.</SubText>
+            <br />
+            <ModalBtn onClick={onChangeSendMail}>인증 메일 발송</ModalBtn>
+          </ModalWrapper>
+        )}
+        {sendMail && (
+          <form>
+            <ModalWrapper>
+              <MainText>이메일을 확인해주세요.</MainText>
+              <SubText>*수신함에서 인증메일을 찾을 수 없을 경우 스팸함을 조회하세요.</SubText>
+              <TextInput placeholder="인증번호를 입력해주세요." type="text"></TextInput>
+              <ModalBtn onClick={onChangeSendMail}>인증 번호 확인</ModalBtn>
+            </ModalWrapper>
+          </form>
+        )}
       </Container>
     </>
   );
