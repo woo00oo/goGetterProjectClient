c13f2e31fc335fef3856c4ebf93ea088d6281b78
seoyoungsoo <iulove37@naver.com>
2021-06-22 20:16:48 +0900

    Feat #7: 상태 관리 및 유효성 검사 추가
    비밀번호 까지 진행

--- pages/SignUp/SignUp2/index.jsx
+++ pages/SignUp/SignUp2/index.jsx
@@ -15,6 +15,7 @@ import {
   Email,
   SignUpTitle,
   SignUpDiv,
+  Error,
 } from '@pages/SignUp/SignUp2/styles';
 import Footer from '@layouts/Footer';
 import AuthModal from '@components/Modal/Auth';
@@ -22,6 +23,12 @@ import ServiceModal from '@components/Modal/Service';
 
 const SignUp2 = () => {
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [nickName, setNickName] = useState('');
+  const [mismatchError, setMismatchError] = useState(false);
 
   const [authModalOpen, setAuthModalOpen] = useState(false);
   const [serviceModalOpen, setServiceModalOpen] = useState(false);
@@ -31,7 +38,23 @@ const SignUp2 = () => {
       e.preventDefault();
       setEmail(e.target.value);
     },
-    [email, setEmail],
+    [email],
+  );
+
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value !== passwordCheck);
+    },
+    [passwordCheck],
+  );
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setMismatchError(e.target.value !== password);
+    },
+    [password],
   );
 
   const onClickAuthButton = useCallback(
@@ -41,6 +64,11 @@ const SignUp2 = () => {
     },
     [email],
   );
+
+  const onClickServiceButton = useCallback((e) => {
+    e.preventDefault();
+    setServiceModalOpen(true);
+  }, []);
 
   return (
     <div id="container" style={{ height: '100%' }}>
@@ -58,10 +86,12 @@ const SignUp2 = () => {
                 </Label>
                 <br></br>
                 <EmailInput
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="이메일을 입력해주세요."
                   onChange={onChangeEmail}
                   value={email}
-                  placeholder="이메일을 입력해주세요."
-                  type="email"
                 ></EmailInput>
                 <AuthButton onClick={onClickAuthButton}>인증받기</AuthButton>
               </Email>
@@ -71,7 +101,14 @@ const SignUp2 = () => {
                 비밀번호<span> *</span>
               </Label>
               <br></br>
-              <TextInput placeholder="비밀번호를 입력해주세요." type="password"></TextInput>
+              <TextInput
+                type="password"
+                id="password"
+                name="password"
+                placeholder="비밀번호를 입력해주세요."
+                onChange={onChangePassword}
+                value={password}
+              ></TextInput>
               <SmallText>비밀번호는 8~16자, 영문, 숫자, 특수문자를 포함해야 합니다.</SmallText>
             </FormGroup>
             <FormGroup>
@@ -79,54 +116,68 @@ const SignUp2 = () => {
                 비밀번호 확인<span> *</span>
               </Label>
               <br></br>
-              <TextInput placeholder="비밀번호를 다시 입력해주세요." type="password"></TextInput>
+              <TextInput
+                type="password"
+                id="password-check"
+                name="password-check"
+                placeholder="비밀번호를 다시 입력해주세요."
+                onChange={onChangePasswordCheck}
+                value={passwordCheck}
+              ></TextInput>
+              {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
             </FormGroup>
             <FormGroup>
               <Label>
                 이름<span> *</span>
               </Label>
               <br></br>
-              <TextInput placeholder="이름을 입력해주세요." type="text"></TextInput>
+              <TextInput
+                type="text"
+                id="name"
+                name="name"
+                placeholder="이름을 입력해주세요."
+                // onChange={onChangeName}
+                // value={name}
+              ></TextInput>
             </FormGroup>
             <FormGroup>
               <Label>
                 휴대폰 번호<span> *</span>
               </Label>
               <br></br>
-              <TextInput placeholder="-없이 번호를 입력해주세요." type="text"></TextInput>
+              <TextInput
+                type="text"
+                id="phone-number"
+                name="phone-number"
+                placeholder="-없이 번호를 입력해주세요."
+                // onChange={onChangePhoneNumber}
+                // value={phoneNumber}
+              ></TextInput>
             </FormGroup>
             <FormGroup>
               <Label>
                 닉네임<span> *</span>
               </Label>
               <br></br>
-              <TextInput placeholder="닉네임을 입력해주세요." type="text"></TextInput>
+              <TextInput
+                type="text"
+                id="nick-name"
+                name="nick-name"
+                placeholder="닉네임을 입력해주세요."
+                // onChange={onChangeNickName}
+                // value={nickName}
+              ></TextInput>
             </FormGroup>
             <FormCheck>
               <div>
                 <input type="checkbox"></input>
                 <CheckText>
-                  우도독의{' '}
-                  <a
-                    onClick={(e) => {
-                      setServiceModalOpen(true);
-                      e.preventDefault();
-                    }}
-                  >
-                    이용약관 및 개인정보 처리 방침
-                  </a>
+                  우도독의 <a onClick={onClickServiceButton}>이용약관 및 개인정보 처리 방침</a>
                   (필수)에 동의합니다.<br></br>
                 </CheckText>
                 <input type="checkbox"></input>
                 <CheckText>
-                  <a
-                    onClick={(e) => {
-                      setServiceModalOpen(true);
-                      e.preventDefault();
-                    }}
-                  >
-                    마케팅 정보 수신
-                  </a>
+                  <a onClick={onClickServiceButton}>마케팅 정보 수신</a>
                   (선택)에 동의합니다.
                 </CheckText>
               </div>
