aaad7287ecc0dbcca92b896af0d43b2fde821de9
DESKTOP-PUHRM72\sodao <sodaoov@gmail.com>
2021-06-22 17:24:01 +0900

    Style #22: 토론게시판 상세게시판 style 수정

--- pages/Discussion/DCContent/styles.jsx
+++ pages/Discussion/DCContent/styles.jsx
@@ -16,17 +16,47 @@ export const SubHeader = styled.h2`
 `;
 
 export const MainContainer = styled.div`
-  border: 1px solid #939597;
   height: 500px;
 `;
 
 export const SubTitle = styled.div`
   font-size: 24px;
-  padding: 20px;
+  padding: 20px 20px;
+  font-weight: bold;
 `;
 
-export const SubMeta = styled.div``;
+export const SubMeta = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid #939597;
+`;
 
-export const SubMetaLeft = styled.div``;
+export const SubMetaLeft = styled.div`
+  font-size: 15px;
+  padding: 10px;
+  color: #939597;
+  & span.comment {
+    color: #0f4c81;
+  }
 
-export const SubMetaRight = styled.div``;
+  & span.divide::after {
+    padding: 10px;
+    content: '|';
+  }
+`;
+
+export const SubMetaRight = styled.div`
+  font-size: 18px;
+  margin-bottom: 20px;
+
+  & i {
+    font-size: 35px;
+    padding: 0px 30px 0 10px;
+  }
+`;
+
+export const SubArticle = styled.article`
+  padding: 30px;
+  font-size: 18px;
+  line-height: 30px;
+`;
