1a598f755092da681d3a63c8e7a47d7e78facd03
DESKTOP-PUHRM72\sodao <sodaoov@gmail.com>
2021-07-03 14:28:17 +0900

    Feat #61: 토론 게시판 작성하기 기능 구현

--- components/DiscussionWriteForm/index.jsx
+++ components/DiscussionWriteForm/index.jsx
@@ -2,33 +2,35 @@ import React, { useState, useCallback } from 'react';
 import { Container, Form, WriteHeader, Input, TextArea, Button } from '@components/DiscussionWriteForm/styles';
 import useInput from '@hooks/useInput';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 const WriteForm = () => {
+  const users = useSelector((state) => state.auth.user);
   const [title, onChangeTitle] = useInput('');
   const [content, onChangeContent] = useInput('');
 
-  const onSubmit = useCallback(
-    (e) => {
-      e.preventDefault();
-      axios
-        .post('/api/user/discussions', {
-          title,
-          content,
-        })
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((err) => {
-          console.dir(err);
-        });
-    },
-    [title, content],
-  );
-
+  const onSubmit = (e) => {
+    console.log('LLLLL');
+    console.log(users.user_id);
+    // e.preventDefault();
+    axios
+      .post('/api/users/discussions?userId=' + users.user_id, {
+        content: content,
+        title: title,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.dir(err);
+      });
+  };
   return (
-    <div>
+    <div style={{ height: '100%' }}>
       <Container>
         <WriteHeader>게시글 작성</WriteHeader>
-        <Form onSubmit={onSubmit}>
+        <Form>
           <div id="title">
             제목
             <Input id="title" name="title" placeholder="제목을 입력해주세요." onChange={onChangeTitle} value={title} />
@@ -43,7 +45,10 @@ const WriteForm = () => {
               value={content}
             />
           </div>
-          <Button type="submit">작성하기</Button>
+          <Button type="submit" onClick={onSubmit}>
+            {/* <Link to="/discussion">작성하기</Link> */}
+            작성하기
+          </Button>
         </Form>
       </Container>
     </div>
