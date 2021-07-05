a2581e70664ab967ec72220f974bff06664298ac
seoyoungsoo <iulove37@naver.com>
2021-07-05 21:19:00 +0900

    Feat #57: 공유 게시판 View 구현

--- pages/ShareBoard/index.jsx
+++ pages/ShareBoard/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Header from '@layouts/Header';
 import Footer from '@layouts/Footer';
-import { Container, Title } from '@pages/ShareBoard/styles';
+import { Container, Title, Content, PopularCards, SharedCards } from '@pages/ShareBoard/styles';
+import Cards from '@components/Cards';
+import CardsPopular from '@components/CardsPopular';
+import { Row } from 'antd';
 
 const ShareBoard = () => {
   return (
@@ -9,6 +12,29 @@ const ShareBoard = () => {
       <Container>
         <Header />
         <Title>공유 게시판</Title>
+        <Content>
+          <h4>인기 게시물</h4>
+          {/* 인기 게시물 grid */}
+          <PopularCards>
+            <Row gutter={[64, 8]}>
+              <CardsPopular />
+              <CardsPopular />
+              <CardsPopular />
+            </Row>
+          </PopularCards>
+          <h4>공유된 게시물</h4>
+          <SharedCards>
+            <Row gutter={[16, 32]}>
+              <Cards />
+              <Cards />
+              <Cards />
+              <Cards />
+              <Cards />
+              <Cards />
+              <Cards />
+            </Row>
+          </SharedCards>
+        </Content>
       </Container>
       <Footer />
     </div>
