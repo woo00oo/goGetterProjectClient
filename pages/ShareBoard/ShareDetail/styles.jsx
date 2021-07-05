f851bda6c722744eeee4f315dd730708844dc082
seoyoungsoo <iulove37@naver.com>
2021-07-05 21:21:27 +0900

    Style #66: 공유게시판 상세보기 css 추가

---
+++ pages/ShareBoard/ShareDetail/styles.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 1200px;
+  position: relative;
+  left: 50%;
+  transform: translate(-50%);
+  margin-bottom: 100px;
+  min-height: 100%;
+`;
+
+export const Title = styled.h2`
+  padding: 50px 30px;
+  border-bottom: 1px solid #939597;
+  margin-bottom: 30px;
+`;
