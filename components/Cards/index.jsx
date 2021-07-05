d749acd17e5ffa6448c397679f5c9252f75cb357
seoyoungsoo <iulove37@naver.com>
2021-07-05 21:19:54 +0900

    Feat #65: 카드 컴포넌트 구현
    - 기본 게시물로 한 열에 최대 4개씩 위치하도록 설정

---
+++ components/Cards/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container, Title, Content, BookTitle, DateTime, Detail, Tag } from '@components/Cards/styles';
+import { Link, Redirect } from 'react-router-dom';
+import { Col } from 'antd';
+import 'antd/dist/antd.css';
+
+const Cards = (props) => {
+  return (
+    <Col span={6}>
+      <Container>
+        {/* <Link to="/shareboard/detail" style={{ textDecoration: 'none', color: '#000', hover: '#000' }}></Link> */}
+        <div class="card" style={{ borderRadius: '5px' }}>
+          {/* <div class="card-header">My Card</div> */}
+          <img style={{ borderTopLeftRadius: '5px', borderTopRightRadius: '5px' }} src="images/book_img.jpg" alt="" />
+          <div class="card-body" style={{ textAlign: 'right' }}>
+            <Title>글 제목</Title>
+            <Content>
+              <BookTitle>책 제목</BookTitle>
+              <DateTime>작성 날짜</DateTime>
+              <Tag>태그</Tag>
+            </Content>
+            <Link to="/shareboard/detail">
+              <Detail>자세히</Detail>
+            </Link>
+          </div>
+        </div>
+      </Container>
+    </Col>
+  );
+};
+
+export default Cards;
