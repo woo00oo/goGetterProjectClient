d2f5c8366354170cfaf566f7dc9dea97535aad21
seoyoungsoo <iulove37@naver.com>
2021-07-05 21:19:13 +0900

    Style #57: 공유게시판 css 추가

--- pages/ShareBoard/styles.jsx
+++ pages/ShareBoard/styles.jsx
@@ -6,9 +6,32 @@ export const Container = styled.div`
   left: 50%;
   transform: translate(-50%);
   margin-bottom: 100px;
-  min-height: 100%;
+  padding-bottom: 300px;
 `;
 
 export const Title = styled.h2`
   padding: 50px 30px;
+  border-bottom: 1px solid #939597;
+  margin-bottom: 30px;
+`;
+
+export const Content = styled.div`
+  padding: 0px 30px;
+  min-height: 100%;
+
+  & h4 {
+    font-weight: bold;
+    margin-bottom: 30px;
+  }
+`;
+
+export const PopularCards = styled.div`
+  padding: 0px 30px;
+  margin-bottom: 30px;
+  margin-left: 30px;
+`;
+
+export const SharedCards = styled.div`
+  padding: 0px 30px;
+  margin-bottom: 30px;
 `;
