9371565179222a0e0d3dfbd406454e848b64bbad
seoyoungsoo <iulove37@naver.com>
2021-07-06 21:33:32 +0900

    Feat #66: 공유게시판 상세보기 내용 / 댓글 컴포넌트 추가

--- pages/ShareBoard/ShareDetail/index.jsx
+++ pages/ShareBoard/ShareDetail/index.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Header from '@layouts/Header';
 import Footer from '@layouts/Footer';
-import { Container, Title } from '@pages/ShareBoard/ShareDetail/styles';
+import { Container, Title, Content } from '@pages/ShareBoard/ShareDetail/styles';
 import axios from 'axios';
 import { useParams } from 'react-router';
+import ShareDetailContent from '@components/ShareDetailContent';
+import ShareBoardReply from '@components/ShareBoardReply';
 
 const ShareDetail = (props) => {
   const { boardId } = useParams();
@@ -14,11 +16,16 @@ const ShareDetail = (props) => {
       console.log(res);
     });
   }, []);
+
   return (
     <div style={{ height: '100%' }}>
       <Container>
         <Header />
         <Title>디테일</Title>
+        <Content>
+          <ShareDetailContent />
+          <ShareBoardReply />
+        </Content>
       </Container>
       <Footer />
     </div>
