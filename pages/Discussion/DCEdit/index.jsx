18f4d2cbab64115fdc27ab4611c7d886e6805428
DESKTOP-PUHRM72\sodao <sodaoov@gmail.com>
2021-07-06 19:32:31 +0900

    Feat #67: 수정하기 버튼 클릭시 props 넘겨주기

--- pages/Discussion/DCEdit/index.jsx
+++ pages/Discussion/DCEdit/index.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '@layouts/Header';
 import Footer from '@layouts/Footer';
 import DiscussionEditForm from '@components/DiscussionEditForm';
 import { Container, DCContainer } from '@pages/Discussion/DCEdit/styles';
 
 const DCEdit = (props) => {
-  console.log(props);
-  const title = props.location.state.title;
-  const content = props.location.state.content;
+  console.log(props.location.state);
+  // const [title, setTitle] = useState(props.location.state.title);
+  // const [content, setContent] = useState(props.location.state.content);
+  // const title = props.location.state.title;
+  // const content = props.location.state.content;
   const Id = props.match.params.id;
+
+  // console.log(title, content);
   return (
     <div style={{ height: '100%' }}>
       <DCContainer>
         <Header />
         <Container>
-          <DiscussionEditForm title={title} content={content} Id={Id}></DiscussionEditForm>
+          <DiscussionEditForm Id={Id}></DiscussionEditForm>
+          {/* title={title} content={content} Id={Id} */}
         </Container>
       </DCContainer>
       <Footer />
