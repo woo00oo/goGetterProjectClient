f65c6f531042342d0bcef5a21ad815272d310a9d
seoyoungsoo <iulove37@naver.com>
2021-07-08 19:27:07 +0900

    Feat #62: 게시판에 따른 페이징 변환

--- components/Paging/index.jsx
+++ components/Paging/index.jsx
@@ -2,18 +2,30 @@ import React, { useState, useEffect } from 'react';
 import '@components/Paging/styles.css';
 import Pagination from 'react-js-pagination';
 
-const Paging = ({ totalElements, currentPage, handlePageChange }) => {
+const Paging = ({ bookBoard = null, totalElements, currentPage, handlePageChange }) => {
   return (
     <div>
-      <Pagination
-        activePage={currentPage + 1}
-        itemsCountPerPage={7}
-        totalItemsCount={totalElements}
-        pageRangeDisplayed={10}
-        prevPageText={'<'}
-        nextPageText={'>'}
-        onChange={handlePageChange}
-      />
+      {bookBoard ? (
+        <Pagination
+          activePage={currentPage + 1}
+          itemsCountPerPage={8}
+          totalItemsCount={totalElements}
+          pageRangeDisplayed={5}
+          prevPageText={'<'}
+          nextPageText={'>'}
+          onChange={handlePageChange}
+        />
+      ) : (
+        <Pagination
+          activePage={currentPage + 1}
+          itemsCountPerPage={7}
+          totalItemsCount={totalElements}
+          pageRangeDisplayed={10}
+          prevPageText={'<'}
+          nextPageText={'>'}
+          onChange={handlePageChange}
+        />
+      )}
     </div>
   );
 };
