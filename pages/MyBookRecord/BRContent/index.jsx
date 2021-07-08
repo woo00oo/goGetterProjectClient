acbc2261016bbd676fe12a05af9af9344ed0af1d
seoyoungsoo <iulove37@naver.com>
2021-07-08 22:00:40 +0900

    Feat #81: 독서 기록 상세 페이지 API 구현 진행

--- pages/MyBookRecord/BRContent/index.jsx
+++ pages/MyBookRecord/BRContent/index.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '@layouts/Header';
 import Footer from '@layouts/Footer';
 import { Container, BRContainer, MyHeader } from '@pages/MyBookRecord/BRContent/styles';
 import RecordDetailContent from '@components/RecordDetailContent';
+import { useParams } from 'react-router';
+import axios from 'axios';
 
 const BRContent = () => {
+  const { boardId } = useParams();
+  // useEffect(() => {
+  //   axios.get();
+  // }, []);
+
   return (
     <div style={{ height: '100%' }}>
       <BRContainer>
