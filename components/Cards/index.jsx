de1be72ecef6096accddc24048fe9a3e9aa07b3d
seoyoungsoo <iulove37@naver.com>
2021-07-09 00:49:56 +0900

    Fix #65: 카드 클릭 오류 수정
    독서 공유 게시판에서 클릭 시 독서 기록 게시판 상세화면으로 넘어가는 오류 수정

--- components/Cards/index.jsx
+++ components/Cards/index.jsx
@@ -3,6 +3,7 @@ import { Container, Title, Content, BookTitle, DateTime, Detail, Tag } from '@co
 import { Link, Redirect } from 'react-router-dom';
 import { Col } from 'antd';
 import 'antd/dist/antd.css';
+import { useSelector } from 'react-redux';
 
 const Cards = (props) => {
   // const onClick = useCallback(() => {
@@ -11,34 +12,64 @@ const Cards = (props) => {
   // }, []);
 
   // console.log(props);
+  const shareBoard = props.share;
   return (
     <Col span={6}>
-      <Container>
-        <Link
-          to={props.shareBoards ? `/shareboard/detail/${props.boardId}` : `/mybookrecord/detail/${props.boardId}`}
-          style={{ textDecoration: 'none', color: '#000', hover: 'none' }}
-        >
-          <div class="card" style={{ borderRadius: '5px' }}>
-            {/* <div class="card-header">My Card</div> */}
-            <img
-              style={{ borderTopLeftRadius: '5px', borderTopRightRadius: '5px', maxHeight: '250px' }}
-              src="images/placeholder.png"
-              alt=""
-            />
-            <div class="card-body" style={{ textAlign: 'right' }}>
-              <Title>{props.title.slice(0, 10) + '...'}</Title>
-              <Content>
-                <BookTitle>{props.bookTitle ? props.bookTitle : '책 제목 없음'}</BookTitle>
-                <DateTime>{props.createdBoard}</DateTime>
-                <Tag>태그</Tag>
-              </Content>
-              {/* <Link to="/shareboard/detail">
+      {shareBoard ? (
+        <Container>
+          <Link
+            to={`/shareboard/detail/${props.boardId}`}
+            style={{ textDecoration: 'none', color: '#000', hover: 'none' }}
+          >
+            <div class="card" style={{ borderRadius: '5px' }}>
+              {/* <div class="card-header">My Card</div> */}
+              <img
+                style={{ borderTopLeftRadius: '5px', borderTopRightRadius: '5px', maxHeight: '250px' }}
+                src="images/placeholder.png"
+                alt=""
+              />
+              <div class="card-body" style={{ textAlign: 'right' }}>
+                <Title>{props.title.slice(0, 10) + '...'}</Title>
+                <Content>
+                  <BookTitle>{props.bookTitle ? props.bookTitle : '책 제목 없음'}</BookTitle>
+                  <DateTime>{props.createdBoard}</DateTime>
+                  <Tag>{props.bookTag ? props.bookTag : ''}</Tag>
+                </Content>
+                {/* <Link to="/shareboard/detail">
               <Detail>자세히</Detail>
             </Link> */}
+              </div>
             </div>
-          </div>
-        </Link>
-      </Container>
+          </Link>
+        </Container>
+      ) : (
+        <Container>
+          <Link
+            to={`/mybookrecord/detail/${props.boardId}`}
+            style={{ textDecoration: 'none', color: '#000', hover: 'none' }}
+          >
+            <div class="card" style={{ borderRadius: '5px' }}>
+              {/* <div class="card-header">My Card</div> */}
+              <img
+                style={{ borderTopLeftRadius: '5px', borderTopRightRadius: '5px', maxHeight: '250px' }}
+                src="images/placeholder.png"
+                alt=""
+              />
+              <div class="card-body" style={{ textAlign: 'right' }}>
+                <Title>{props.title.slice(0, 10) + '...'}</Title>
+                <Content>
+                  <BookTitle>{props.bookTitle ? props.bookTitle : '책 제목 없음'}</BookTitle>
+                  <DateTime>{props.createdBoard}</DateTime>
+                  <Tag>{props.bookTag ? props.bookTag : ''}</Tag>
+                </Content>
+                {/* <Link to="/shareboard/detail">
+              <Detail>자세히</Detail>
+            </Link> */}
+              </div>
+            </div>
+          </Link>
+        </Container>
+      )}
     </Col>
   );
 };
