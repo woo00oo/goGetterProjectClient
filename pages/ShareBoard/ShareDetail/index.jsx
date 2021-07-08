36f76bafdb2aea755e7205c004805469d3b3a0f5
seoyoungsoo <iulove37@naver.com>
2021-07-09 01:03:15 +0900

    Refactor #66: 글 작성자 추가

--- pages/ShareBoard/ShareDetail/index.jsx
+++ pages/ShareBoard/ShareDetail/index.jsx
@@ -12,14 +12,16 @@ const ShareDetail = (props) => {
   const [post, setPost] = useState([]);
   const [reply, setReply] = useState([]);
   const [writerId, setWriterId] = useState('');
+  const [writerNickName, setWriterNickName] = useState('');
 
   useEffect(() => {
     axios
       .get(`/api/bkuser/sharings?id=${boardId}`)
       .then((res) => {
-        console.log(res.data.data);
+        // console.log(res.data.data);
         setPost(res.data.data);
         setWriterId(res.data.data.writer_info.writer_id);
+        setWriterNickName(res.data.data.writer_info.nick_name);
         // console.log('상세게시판 api', res.data.data);
       })
       .catch((err) => {
@@ -45,7 +47,7 @@ const ShareDetail = (props) => {
     <div style={{ height: '100%' }}>
       <Container>
         <Header />
-        <Title>상세 페이지</Title>
+        <Title>{writerNickName}님의 게시물</Title>
         <Content>
           <ShareDetailContent
             title={post.title}
