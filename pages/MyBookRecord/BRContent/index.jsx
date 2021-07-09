52c4796df45cf32f3244b35ff8f8335c3f6409d1
seoyoungsoo <iulove37@naver.com>
2021-07-10 00:58:33 +0900

    Feat #38: 독서 기록 상세 페이지 수정 삭제 모달 추가

--- pages/MyBookRecord/BRContent/index.jsx
+++ pages/MyBookRecord/BRContent/index.jsx
@@ -4,12 +4,21 @@ import Footer from '@layouts/Footer';
 import { Container, Title, Content } from '@pages/MyBookRecord/BRContent/styles';
 import RecordDetailContent from '@components/RecordDetailContent';
 import { useParams } from 'react-router';
+import EditModal from '@components/Modal/RecordEdit';
+import DeleteModal from '@components/Modal/RecordDelete';
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const BRContent = () => {
+const BRContent = (props) => {
   const { boardId } = useParams();
+  const state = useSelector((state) => state.auth.user);
 
   const [recordDetailData, setRecordDetailData] = useState([]);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+
+  const [successEdit, setSuccessEdit] = useState(false);
+  const [successDelete, setSuccessDelete] = useState(false);
 
   useEffect(() => {
     axios
@@ -25,15 +34,45 @@ const BRContent = () => {
       });
   }, []);
 
-  console.log(recordDetailData);
+  if (successEdit) {
+    setEditModalOpen(false);
+    props.history.push(`/mybookrecord/detail/${boardId}`);
+    window.location.reload();
+    setSuccessEdit(false);
+  }
+
+  if (successDelete) {
+    setDeleteModalOpen(false);
+    props.history.push('/mybookrecord');
+    window.location.reload();
+    setSuccessDelete(false);
+  }
 
   return (
     <div style={{ height: '100%' }}>
+      <EditModal
+        boardId={boardId}
+        post={recordDetailData}
+        editModalOpen={editModalOpen}
+        setEditModalOpen={setEditModalOpen}
+        setSuccessEdit={setSuccessEdit}
+      />
+      <DeleteModal
+        deleteModalOpen={deleteModalOpen}
+        setDeleteModalOpen={setDeleteModalOpen}
+        boardId={boardId}
+        setSuccessDelete={setSuccessDelete}
+        userId={state.user_id}
+      />
       <Container>
         <Header />
         <Title>나의 게시물</Title>
         <Content>
-          <RecordDetailContent recordDetailData={recordDetailData} />
+          <RecordDetailContent
+            recordDetailData={recordDetailData}
+            setEditModalOpen={setEditModalOpen}
+            setDeleteModalOpen={setDeleteModalOpen}
+          />
         </Content>
       </Container>
       <Footer />
