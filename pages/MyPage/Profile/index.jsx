6b8983f0070c25a92af62bd8f615163fe7a0ddaf
seoyoungsoo <iulove37@naver.com>
2021-07-10 00:59:11 +0900

    Feat #88: 마이페이지 프로필 수정 API 구현

--- pages/MyPage/Profile/index.jsx
+++ pages/MyPage/Profile/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Header from '@layouts/Header';
 import Footer from '@layouts/Footer';
 import Tab from '@components/Tab';
@@ -17,8 +17,107 @@ import {
   MyPageDiv,
   ModifiedBtn,
 } from '@pages/MyPage/Profile/styles';
+import { Error } from '@pages/SignUp/SignUp2/styles';
+import { useSelector } from 'react-redux';
+import useInput from '@hooks/useInput';
+import pwEncrypt from '@utils/pwEncrypt';
+import axios from 'axios';
+import { withRouter } from 'react-router';
 
-const MyPageProfile = () => {
+const MyPageProfile = (props) => {
+  const pwCheck = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/;
+  const nickNameCheck = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{4,16}$/;
+
+  const userId = useSelector((state) => state.auth.user.user_id);
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [nickName, onChangeNickName, setNickName] = useInput('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+
+  const [mismatchError, setMismatchError] = useState(false);
+
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value !== passwordCheck);
+    },
+    [passwordCheck],
+  );
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setMismatchError(e.target.value !== password);
+    },
+    [password],
+  );
+
+  useEffect(() => {
+    axios
+      .get(`/api/bkusers/mypage/${userId}`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data.data);
+        const data = res.data.data;
+        setNickName(data.nick_name);
+        setEmail(data.email);
+        setName(data.name);
+        setPhoneNumber(data.phone_number);
+      })
+      .catch((err) => {
+        console.dir(err);
+      });
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      if (mismatchError) {
+        alert('비밀번호를 확인해주세요.');
+      } else if (!pwCheck.test(password)) {
+        alert('비밀번호를 확인해주세요.');
+      } else if (!nickNameCheck.test(nickName)) {
+        alert('닉네임을 확인해주세요.');
+      } else {
+        const pwHash = pwEncrypt(password);
+        axios
+          .patch(
+            `/api/bkusers/mypage/edit/${userId}`,
+            // {
+            //   headers: {
+            //     'Access-Control-Allow-Origin': '*',
+            //   },
+            // },
+            {
+              nick_name: nickName,
+              password: pwHash,
+            },
+            {
+              withCredentials: true,
+            },
+          )
+          .then((res) => {
+            console.log(res);
+            alert('성공적으로 수정되었습니다.');
+            props.history.push(`/mypage/profile/${userId}`);
+            window.location.reload();
+          })
+          .then((err) => {
+            console.dir(err);
+          });
+      }
+    },
+    [mismatchError, password, nickName, pwCheck, nickNameCheck, userId],
+  );
+
+  if (!email) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div id="mypage-profile" style={{ height: '100%' }}>
       <MyPageDiv>
@@ -31,31 +130,43 @@ const MyPageProfile = () => {
               <Profile>
                 <CantModified>
                   이메일
-                  <span>example@gmail.com</span>
+                  <span>{email}</span>
                   이름
-                  <span>홍길동</span>
+                  <span>{name}</span>
                   휴대폰
-                  <span>01000000000</span>
+                  <span>{phoneNumber}</span>
                 </CantModified>
                 <ProfileImage>
                   <i className="fas fa-user-circle fa-8x"></i>
                 </ProfileImage>
               </Profile>
-              <ProfileForm>
+              <ProfileForm onSubmit={onSubmit}>
                 <FormGroup>
                   <Label>닉네임</Label>
-                  <Input placeholder="닉네임을 입력해주세요." type="text"></Input>
+                  <Input type="text" placeholder={nickName} value={nickName} onChange={onChangeNickName}></Input>
+                  {!nickName && <Error>닉네임을 입력해주세요.</Error>}
                 </FormGroup>
                 <FormGroup>
                   <Label>비밀번호</Label>
-                  <Input placeholder="비밀번호를 입력해주세요." type="password"></Input>
+                  <Input
+                    placeholder="비밀번호를 입력해주세요."
+                    type="password"
+                    value={password}
+                    onChange={onChangePassword}
+                  ></Input>
                   <SmallText>비밀번호는 8~16자, 영문, 숫자, 특수문자를 포함해야 합니다.</SmallText>
                 </FormGroup>
                 <FormGroup>
                   <Label>비밀번호 확인</Label>
-                  <Input placeholder="비밀번호를 다시 입력해주세요." type="password"></Input>
+                  <Input
+                    placeholder="비밀번호를 다시 입력해주세요."
+                    type="password"
+                    value={passwordCheck}
+                    onChange={onChangePasswordCheck}
+                  ></Input>
+                  {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
                 </FormGroup>
-                <ModifiedBtn>수정하기</ModifiedBtn>
+                <ModifiedBtn type="submit">수정하기</ModifiedBtn>
               </ProfileForm>
             </ProfileDiv>
           </ContentDiv>
@@ -66,4 +177,4 @@ const MyPageProfile = () => {
   );
 };
 
-export default MyPageProfile;
+export default withRouter(MyPageProfile);
