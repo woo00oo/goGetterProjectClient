03168bb860687ffd30153372354acb912a7da23b
seoyoungsoo <iulove37@naver.com>
2021-07-10 00:56:01 +0900

    Feat #87: 독서 공유 글 작성 페이지 API 통신 구현

--- components/ShareBoardWriteForm/index.jsx
+++ components/ShareBoardWriteForm/index.jsx
@@ -2,22 +2,62 @@ import React, { useState, useCallback } from 'react';
 import { Container, Form, WriteHeader, Input, TextArea, Button, FormDiv } from '@components/ShareBoardWriteForm/styles';
 import { useSelector } from 'react-redux';
 import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { Redirect } from 'react-router';
 
-const ShareBoardWriteForm = () => {
+const ShareBoardWriteForm = (props) => {
   const [title, onChangeTitle] = useInput('');
   const [bookTitle, onChangeBookTitle] = useInput('');
   const [content, onChangeContent] = useInput('');
   const [tag, onChangeTag] = useInput('');
 
+  const [createSuccess, setCreateSuccess] = useState(false);
+
   const userData = useSelector((state) => state.auth.user);
+  const userId = userData.user_id;
 
   const onSubmit = useCallback(
     (e) => {
       e.preventDefault();
-      console.log(title, bookTitle, content, tag);
+      console.log(title, bookTitle, content, tag, userId);
+
+      if (!title.length || !title.trim().length) {
+        alert('글 제목을 작성해주세요.');
+      } else if (!bookTitle.length || !bookTitle.trim().length) {
+        alert('책 제목을 작성해주세요.');
+      } else if (!content.length || !content.trim().length) {
+        alert('내용을 작성해주세요.');
+      } else {
+        axios
+          .post(
+            '/api/user/sharings',
+            {
+              book_title: bookTitle,
+              content,
+              sharing_board_tag: tag,
+              title,
+              user_id: userId,
+            },
+            {
+              withCredentials: true,
+            },
+          )
+          .then((res) => {
+            console.log(res);
+            alert('성공적으로 작성되었습니다.');
+            setCreateSuccess(true);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     },
-    [title, bookTitle, content, tag],
+    [title, bookTitle, content, tag, userId],
   );
+
+  if (createSuccess) {
+    return <Redirect to="/shareboard" />;
+  }
 
   return (
     <div>
