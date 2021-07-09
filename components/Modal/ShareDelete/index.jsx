b7f539b2213a4cffcd9f04e6e0b0a98ca6687a45
seoyoungsoo <iulove37@naver.com>
2021-07-10 01:05:57 +0900

    Feat #89: 독서 공유 글 삭제 모달 구현

---
+++ components/Modal/ShareDelete/index.jsx
@@ -0,0 +1,60 @@
+import React, { useCallback } from 'react';
+import {
+  Modal,
+  ModalBtn,
+  OKBtn,
+  NoBtn,
+  ModalWrapper,
+  MainText,
+  SubText,
+  Container,
+} from '@components/Modal/Check/styles';
+import axios from 'axios';
+
+const ShareDelete = (props) => {
+  // console.log(props);
+
+  const onClickDelete = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      axios
+        .delete(`/api/user/sharings?boardId=${props.boardId}&userId=${props.userId}`, {
+          withCredentials: true,
+        })
+        .then((res) => {
+          alert('삭제되었습니다.');
+          props.setSuccessDelete(true);
+        })
+        .catch((err) => {
+          console.dir(err);
+        });
+    },
+    [props],
+  );
+
+  return (
+    <>
+      <Modal checkModal={props.deleteModalOpen} />
+      <Container checkModal={props.deleteModalOpen}>
+        <ModalWrapper>
+          <MainText>삭제하시겠습니까?</MainText>
+          <SubText></SubText>
+          <ModalBtn>
+            <NoBtn
+              onClick={() => {
+                props.setDeleteModalOpen(false);
+              }}
+            >
+              취소하기
+            </NoBtn>
+
+            <OKBtn onClick={onClickDelete}>삭제하기</OKBtn>
+          </ModalBtn>
+        </ModalWrapper>
+      </Container>
+    </>
+  );
+};
+
+export default ShareDelete;
