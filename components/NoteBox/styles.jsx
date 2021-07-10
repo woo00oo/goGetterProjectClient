9b74eeb6ec8e3c16ae34a9f1f8d24943c2ee06bd
seoyoungsoo <iulove37@naver.com>
2021-07-10 17:58:59 +0900

    Style #82: 쪽지함 전체 조회 View css

---
+++ components/NoteBox/styles.jsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  margin: 0 0 0 30px;
+  width: 330px;
+  height: 100%;
+  border: 1px solid #ced4da;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  border-bottom: none;
+
+  & h4 {
+    margin: 0;
+    font-size: 24px;
+    padding: 30px 20px;
+    border-bottom: 1px solid #ced4da;
+  }
+`;
