43f5341e91baabf80a8f40496a98ea2543b60fce
DESKTOP-PUHRM72\sodao <sodaoov@gmail.com>
2021-07-28 15:59:56 +0900

    Feat #106: 관리자 페이지 사이드바 수정

--- components/AdminTab/index.jsx
+++ components/AdminTab/index.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { LinkClicked } from '@components/AdminTab/styles';
 import { Link, useHistory } from 'react-router-dom';
 import { TabDiv } from '@components/AdminTab/styles';
 
 const Tab = () => {
+  const pathname = window.location.pathname;
   const history = useHistory();
-  const onClick = (e) => {
-    window.location.reload();
+  const onClickAdmin = (e) => {
     history.push('/admin');
+    window.location.reload();
+  };
+
+  const onClickMonthly = (e) => {
+    history.push('/admin/monthly');
+    window.location.reload();
   };
   return (
     <TabDiv>
       <h2>관리자 페이지</h2>
       <ul>
-        <li className="select" onClick={onClick}>
-          회원 관리
-        </li>
+        <LinkClicked isActive={!pathname.includes('/admin/month')}>
+          <li onClick={onClickAdmin}>회원 관리</li>
+        </LinkClicked>
+        <LinkClicked isActive={pathname.includes('/admin/month')}>
+          <li onClick={onClickMonthly}>월별 회원가입 수</li>
+        </LinkClicked>
       </ul>
     </TabDiv>
   );
