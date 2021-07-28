46f4d24acd262df07bad3e2591566763e02424d2
seoyoungsoo <iulove37@naver.com>
2021-07-28 18:30:51 +0900

    Choer #noissue: 웹팩 설정 변경

--- webpack.config.js
+++ webpack.config.js
@@ -62,18 +62,18 @@ const config = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
-    publicPath: '/dist/',
+    publicPath: '/',
   },
   devServer: {
     historyApiFallback: true,
     port: 3000,
-    publicPath: '/dist/',
+    host: '0.0.0.0',
     proxy: {
-      '/api/': {
+      '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
-      '/chat/': {
+      '/chat': {
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
