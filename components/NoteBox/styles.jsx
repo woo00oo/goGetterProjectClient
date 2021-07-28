ee6ea1b9f1839450be95faf864f90ab91620f7c1
seoyoungsoo <iulove37@naver.com>
2021-07-28 16:21:04 +0900

    Style #97: 노트박스 css 변경

--- components/NoteBox/styles.jsx
+++ components/NoteBox/styles.jsx
@@ -1,6 +1,12 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Content = styled.div`
   margin: 0 0 0 30px;
   width: 330px;
   height: 100%;
